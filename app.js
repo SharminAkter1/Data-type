76349c7db5d98f6a8754a16ec041ead387a65d9e
Sharmin Akter <72566503+SharminAkter1@users.noreply.github.com>
2021-07-22 11:16:08 +0600

    data type

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// String data 
+name ='Sharmin Akter';
+
+//Number 
+const age = 20;
+//
+// Boolean
+const haskid = true;
+//Null
+const car = null;
+
+//Undefined
+let test;
+//symbol
+const sym = Symbol();
+//Referance types-objects
+const array = ['sharmin','Akter'];
+//object literal
+const address = {
+  city:'Bogura',
+  state:'BG'
+}
+
+const today = new Date ();
+console.log(today);
+
+console.log(typeof today);
+
+
+
